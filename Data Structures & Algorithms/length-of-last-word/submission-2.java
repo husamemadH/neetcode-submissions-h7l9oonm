3a77343095a026a1e057cf64a91276f6eacf5965
husamemadH <husamemad60@gmail.com>
2026-04-07 22:18:23 +0300

    Add: length-of-last-word - submission-2

---
+++ Data Structures & Algorithms/length-of-last-word/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+      int answer = 0;
+    
+
+      for(int i = s.length() - 1 ; i >= 0 ; i--)
+        {
+            if(s.charAt(i) == ' ' && answer == 0) {
+                continue;
+            }
+            if(s.charAt(i) == ' ' && answer != 0) {
+                break;
+            }
+            answer++;
+        }
+      return answer;
+    }
+}
