ed84319e932fc3f20935936fb471a6b6a4748530
husamemadH <husamemad60@gmail.com>
2026-04-13 13:01:27 +0300

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        Map<String , List<String>> map = new HashMap<>();
+
+        for(String str : strs) {
+
+            int[] freq = new int[26];
+            for(Character c : str.toCharArray()) {
+                freq[c - 'a']++;
+            }
+            String key = Arrays.toString(freq);
+            map.computeIfAbsent(key, k -> new ArrayList<String>()).add(str);
+
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
