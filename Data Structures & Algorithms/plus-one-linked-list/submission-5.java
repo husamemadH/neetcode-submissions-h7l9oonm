402d80b598ea05d5c2b04cbb0d3927da5fede938
husamemadH <husamemad60@gmail.com>
2026-04-23 20:49:32 +0300

    Add: plus-one-linked-list - submission-5

---
+++ Data Structures & Algorithms/plus-one-linked-list/submission-5.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode plusOne(ListNode head) {
+        ListNode dummy = new ListNode(0 , head);
+        ListNode notNine = dummy;
+
+        while(head != null) {
+            if(head.val != 9) {
+                notNine = head;
+            }
+            head = head.next;
+        }
+
+        notNine.val++;
+        notNine = notNine.next;
+        while(notNine != null) {
+            
+            notNine.val = 0;
+            notNine = notNine.next;
+        }
+        
+        return dummy.val == 0 ? dummy.next : dummy;
+    }
+}
