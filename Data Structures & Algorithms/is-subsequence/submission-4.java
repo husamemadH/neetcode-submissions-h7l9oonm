15dd236d3ef5f471573f60dc16c811b79911f558
husamemadH <husamemad60@gmail.com>
2026-04-27 20:48:42 +0300

    Add: is-subsequence - submission-4

---
+++ Data Structures & Algorithms/is-subsequence/submission-4.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0 , j = 0;
+        if(s.equals("")) {
+            return true;
+        }
+        while(j < t.length()) {
+
+            if(i < s.length() && t.charAt(j) == s.charAt(i)) {
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.length();
+    }
+}
