6097a8da5c4affa24197c19c402f7de75fe78ddc
husamemadH <husamemad60@gmail.com>
2026-05-02 06:20:54 +0300

    Add: crawler-log-folder - submission-2

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        Stack<String> stk = new Stack<>();
+
+        for(String log : logs) {
+
+            if(log.equals("../")) {
+                
+                if(stk.isEmpty()) {
+                     continue;
+                }
+
+                stk.pop();
+
+            }
+
+            else if(log.equals("./")) {
+
+            }
+
+            else{
+                stk.push(log);
+            }
+
+        }
+
+        return stk.size();
+    }
+}
