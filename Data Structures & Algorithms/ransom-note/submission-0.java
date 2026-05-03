09374377ce513ee4c599ffe207f13b22be029007
husamemadH <husamemad60@gmail.com>
2026-05-03 21:23:21 +0300

    Add: ransom-note - submission-0

---
+++ Data Structures & Algorithms/ransom-note/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] freq = new int[26];
+
+        for(char c : magazine.toCharArray()) {
+
+                freq[c - 'a']++;
+
+        }
+
+        for(char c : ransomNote.toCharArray()) {
+            
+                freq[c - 'a']--;
+        }
+
+        for(int n : freq) {
+            if(n < 0 ) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
